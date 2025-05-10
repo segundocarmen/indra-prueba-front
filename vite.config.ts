bcadf187ca21265cf1b8e7dfca1a862dd10b73ee
segundo carmen dominguez <scarmen@bzero.cl>
2025-05-10 09:31:47 -0500

    update project

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'dist',
+        target: 'esnext'
+    },
+    preview: {
+        port: 3000,
+        strictPort: true,
+        host: '0.0.0.0',
+        allowedHosts: true
+    },
     server: {
         host: true,
-        port: 3000
+        port: 3000,
+        allowedHosts: true
     }
 });
